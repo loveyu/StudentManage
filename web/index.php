5d8ffb6653097bb3d7ece6c8827e7816ad9142ee
loveyu <i@loveyu.info>
2018-10-25 03:00:33 +0800

    调整数据库用户名和密码对应的配置文件

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require_once("../sys/config.php");
 c()->getTimer()->setBeginTime($time); //修正启动时间
 unset($time); //删除时间变量
 
-cfg()->load('../config/all.php'); //加载其他配置文件
+cfg()->load(__DIR__.'/../config/all.php'); //加载其他配置文件
 lib()->load('Hook')->add('Hook', new \ULib\Hook())->add(); //加载自定义类
 u()->home(array(
 	'Home',
